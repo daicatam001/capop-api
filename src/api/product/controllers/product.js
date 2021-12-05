e7ae142031daa144233cfe1f77afa8c779b4bda6
tampt <tampt95@gmail.com>
2021-12-05 21:54:23 +0700

    update product schema

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -5,11 +5,14 @@
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
-const { first } = require('lodash/fp');
 module.exports = createCoreController("api::product.product", ({ strapi }) => ({
   async findOneBySlug(ctx) {
-    const { slug, id } = ctx.params;
-    const entity = first(await strapi.service("api::product.product").find({ slug }));
+    const entity = await strapi.db.query("api::product.product").findOne({
+      where: { slug: "cha-muc" },
+      populate: { thumb: true },
+    });
+    console.log(entity);
+
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
     return this.transformResponse(sanitizedEntity);
